1bac70a3b7382f3bdfd07252e64e22f6c3fd3d3a
stemcoders <joseph.parsons@s.stemk12.org>
2019-05-01 15:49:42 -0600

    Added methods to the Vector2f class

--- src/math/Vector2f.java
+++ src/math/Vector2f.java
@@ -24,6 +24,14 @@ public class Vector2f {
         return this;
     }
 
+    public Vector2f add(Vector2f other) {
+        return this.add(other.x, other.y);
+    }
+
+    public Vector2f sub(Vector2f other) {
+        return this.sub(other.x, other.y);
+    }
+
     public Vector2f mul(float x, float y) {
         this.x *= x;
         this.y *= y;
@@ -36,13 +44,38 @@ public class Vector2f {
         return this;
     }
 
+    public Vector2f mul(float f) {
+        return mul(f, f);
+    }
+
+    public Vector2f div(float f) {
+        return div(f, f);
+    }
+
+    public float dot(float x, float y) {
+        return this.x * x + this.y * y;
+    }
+
+    public float dot(Vector2f other) {
+        return dot(other.x, other.y);
+    }
+
+    public float getLength() {
+        return MathUtils.sqrt(this.x * this.x + this.y * this.y);
+    }
+
     public Vector2f normalize() {
-        float mag = MathUtils.sqrt(x * x + y * y);
+        float mag = this.getLength();
+        if (mag == 0) {
+            return this;
+        }
         this.x /= mag;
         this.y /= mag;
         return this;
     }
 
+    public static Vector2f getIVector() { return new Vector2f(1.0f, 0.0f); }
+    public static Vector2f getJVector() { return new Vector2f(0.0f, 1.0f); }
     public static Vector2f getOne() { return new Vector2f(1.0f, 1.0f); }
     public static Vector2f getZero() { return new Vector2f(0.0f, 0.0f); }
 }
